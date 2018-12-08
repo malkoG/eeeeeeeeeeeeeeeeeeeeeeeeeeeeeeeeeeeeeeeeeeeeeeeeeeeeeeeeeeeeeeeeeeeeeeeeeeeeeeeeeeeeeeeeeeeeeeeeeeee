471c51013abf67e043546f90e263b2428c95a911
KODING WARRIOR <rijgndqw012@gmail.com>
2018-12-09 05:58:31 +0900

    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- e.adb
+++ e.adb
@@ -2,5 +2,8 @@ with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Program is
 begin
-   Ada.Text_IO.Put("e");
+   While_Loop:
+       loop
+	  Ada.Text_IO.Put("e");
+       end loop While_Loop;
 end Program;
